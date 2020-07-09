afd972591fd17b3c68c7839ca97c7417af2c8026
Blackstar <baker644@umn.edu>
2020-07-09 16:13:50 -0500

    Added some html files to be updated, but I'm just a little confused about some things and need to talk it out with Charlie

---
+++ app/routes/html-routes.js
@@ -0,0 +1,34 @@
+module.exports = function (app) {
+
+    app.get("/playlists", function (req, res) {
+        res.render("index");
+    });
+
+    app.get("/login", function (req, res) {
+        res.render("login");
+    });
+
+    app.get("/signup", function (req, res) {
+        res.render("signup");
+    });
+
+    app.get("/settings/profile", function (req, res) {
+        res.render("settings");
+    });
+
+    app.get("/:username", async function (req, res) {
+        try {
+            const userExists = await db.User.findAll(
+                { where: { username: req.params.username } }
+            );
+            if (userExists) {
+                res.render("profile");
+            }
+        } catch (err) {
+            // Maybe throw some kind of 'user not found' alert.
+            res.status(404).render("index");
+        }
+    });
+};
+
+module.exports = app;
