9022edf22b13ef95a90d658b284a92aa062213cd
Blackstar <baker644@umn.edu>
2020-07-09 10:08:46 -0500

    Tightened up files and prepared for working framwork.
    Finished up sequelize User method for Gene, cleaned up passport.js, and specified what user properties are exposed in the session

--- app/config/passport.js
+++ app/config/passport.js
@@ -3,46 +3,30 @@ const LocalStrategy = require("passport-local").Strategy;
 
 const db = require("../models");
 
-passport.use(
-    new LocalStrategy(
-        {
-            usernameField: "username"
-        },
-        (email, password, done) => {
-            // When a user tries to sign in this code runs
-            db.User.findOne({
-                where: {
-                    username: username
-                }
-            }).then(dbUser => {
-                // If there's no user with the given email
-                if (!dbUser) {
-                    return done(null, false, {
-                        message: "Incorrect username."
-                    });
-                // Note that if we go with this there needs to be a .validPassword prototype.
-                } else if (!dbUser.validPassword(password)) {
-                    return done(null, false, {
-                        message: "Incorrect password."
-                    });
-                }
-                // If none of the above, return the user
-                return done(null, dbUser);
-            });
-        }
-    )
-);
+// http://www.passportjs.org/docs/configure/ just note that the example is using MongoDB, so findOne syntax is different. I think.
 
-// In order to help keep authentication state across HTTP requests,
-// Sequelize needs to serialize and deserialize the user
-// Just consider this part boilerplate needed to make it all work
+passport.use(new LocalStrategy(
+    (username, password, done) => {
+        db.User.findOne({
+            where: { username: username }
+        }).then(dbUser => {
+            if (!dbUser) {
+                return done(null, false, { message: "Incorrect username." });
+            } else if (!dbUser.validPassword(password)) {
+                return done(null, false, { message: "Incorrect password." });
+            }
+            return done(null, dbUser);
+        });
+    }
+));
+
+// we are chosing to only store these aspects of the user object in the session.
 passport.serializeUser((user, cb) => {
-    cb(null, user);
+    cb(null, {id: user.id, username: user.username, role: user.role});
 });
 
-passport.deserializeUser((obj, cb) => {
-    cb(null, obj);
+passport.deserializeUser((user, cb) => {
+    cb(null, {id: user.id, username: user.username, role: user.role});
 });
 
-// Exporting our configured passport
 module.exports = passport;
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const session = require("express-session");
-// Requiring passport as we've configured it
 const passport = require("./app/config/passport");
 
 const PORT = process.env.PORT || 3000;
@@ -13,18 +12,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// revisit the passport and sessions to make sure we know what is going on. Consider putting the secret in an environment variable just for giggles.
 app.use(session({ secret: "change me later", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
-// https://stackoverflow.com/questions/22052258/what-does-passport-session-middleware-do
 app.use(passport.session());
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// honestly I have absolutely no idea how this syntax works
-// some hard to understand explanations here: https://stackoverflow.com/questions/6059246/how-to-include-route-handlers-in-multiple-files-in-express
-require("./app/routes/html-routes.js")(app);
+require("./routes/html-routes.js")(app);
 require("./app/routes/api-routes.js")(app);
 
 db.sequelize.sync().then(() => {
