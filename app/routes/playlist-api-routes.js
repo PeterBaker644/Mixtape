0940d0132a10030f8f151be9cf63ad781b0c9c72
gshaver <geneshaver@gmail.com>
2020-07-11 20:17:44 -0500

    correcting borked build

--- app/routes/playlist-api-routes.js
+++ app/routes/playlist-api-routes.js
@@ -5,34 +5,31 @@ const passport = require("../config/passport");
 module.exports = function (app) {
     //should this all be async await?
 
-    app.get("/api/playlists", (req, res) => {
-        //this finds all playlists and orders by title.
-        //confirmed working in postman
-        db.Playlist.findAll({
-            order: ["title"]
-        })
-            .then(function (dbPlaylist) {
-                res.json(dbPlaylist);
-            });
-    });
-    app.get("/api/playlists/orderby_newest", (req, res) => {
-        //this finds all playlists and orders by newest.
-        //confirmed working in postman
-        db.Playlist.findAll({
-            order: [
-                ["title", "desc"],
-            ]
-        })
-            .then(function (dbPlaylist) {
-                res.json(dbPlaylist);
-            });
-    });
-<<<<<<< Updated upstream
+    // app.get("/api/playlists", (req, res) => {
+    //     //this finds all playlists and orders by title.
+    //     //confirmed working in postman
+    //     db.Playlist.findAll({
+    //         order: ["title"]
+    //     })
+    //         .then(function (dbPlaylist) {
+    //             res.json(dbPlaylist);
+    //         });
+    // });
+    // app.get("/api/playlists/orderby_newest", (req, res) => {
+    //     //this finds all playlists and orders by newest.
+    //     //confirmed working in postman
+    //     db.Playlist.findAll({
+    //         order: [
+    //             ["title", "desc"],
+    //         ]
+    //     })
+    //         .then(function (dbPlaylist) {
+    //             res.json(dbPlaylist);
+    //         });
+    // });
 
 
 
-    app.get("/api/playlists/include_upvotes", (req, res) => {
-=======
     // old working code
     // app.get("/api/playlists/include_upvotes", (req, res) => {
     //     //this finds all playlists and orders by title.
@@ -56,18 +53,14 @@ module.exports = function (app) {
     //         });
     // });
 
-    // old working code
     app.get("/api/playlists/include_upvotes3", (req, res) => {
->>>>>>> Stashed changes
         //this finds all playlists and orders by title.
         //this also includes upvote table as an array of objects insite the playlist object
         //which is itself inside an array
         //confirmed working in postman
         db.Playlist.findAll({
-<<<<<<< Updated upstream
             include: db.Vote,
-            order: ["title"]
-=======
+            order: ["title"],
             include: [
                 { model: db.Song, attributes: ["song_title", "song_artist"] }
             ],
@@ -80,26 +73,23 @@ module.exports = function (app) {
                 [db.Sequelize.literal(`(SELECT users.username FROM users WHERE id=playlist.Userid)`), "username"],
             ],
             order: db.sequelize.literal("title, song_order ASC"),
->>>>>>> Stashed changes
         })
             .then(function (dbPlaylist) {
                 res.json(dbPlaylist);
             });
     });
 
-<<<<<<< Updated upstream
     //PARAM URLS GO LAST
-    //we want to get all the playlists from a user by userid not by username right?
-    app.get("/api/playlists/:UserId", (req, res) => {
-        //this pulls all the playlists by specific userID.
-        //this is the userID that is in the playlist table that is
-        //a Fkey linked to user table
-        //confirmed working in postman
-        db.Playlist.findAll({
-            where: {
-                UserId: req.params.UserId
-            }
-=======
+    // //we want to get all the playlists from a user by userid not by username right?
+    // app.get("/api/playlists/:UserId", (req, res) => {
+    //     //this pulls all the playlists by specific userID.
+    //     //this is the userID that is in the playlist table that is
+    //     //a Fkey linked to user table
+    //     //confirmed working in postman
+    //     db.Playlist.findAll({
+    //         where: {
+    //             UserId: req.params.UserId
+    //         }
     app.get("/api/playlists/:username", (req, res) => {
         //this finds all playlists and orders by title.
         //this includes username title, and array of songs and orders those songs by song order
@@ -121,14 +111,11 @@ module.exports = function (app) {
             // where: db.sequelize.literal(`(users.username) = ${req.params.username}`),
             // where: {rainbows:req.params.username},
             order: db.sequelize.literal("title, song_order ASC"),
->>>>>>> Stashed changes
         })
             .then(function (dbPlaylist) {
                 res.json(dbPlaylist);
             });
     });
-<<<<<<< Updated upstream
-=======
 
     //old working code
     // app.get("/api/playlists/include_upvotes2", (req, res) => {
@@ -153,7 +140,6 @@ module.exports = function (app) {
     //             res.json(dbPlaylist);
     //         });
     // });
->>>>>>> Stashed changes
 
     //PARAM URLS GO LAST
     //we want to get all the playlists from a user by userid not by username right?
@@ -176,19 +162,19 @@ module.exports = function (app) {
 
 
     // I think we need the authenticate line here. We'll double check when testing though.
-    app.post("/api/playlists", passport.authenticate("local"), (req, res) => {
-        db.Playlist.create({
-            title: req.body.playlistTitle,
-            string: req.body.playlistString,
-        })
-            .then(() => {
-                // I don't know what we'd want to do here.
-                res.status(200);
-            })
-            .catch(err => {
-                res.status(401).json(err);
-            });
-    });
+    // app.post("/api/playlists", passport.authenticate("local"), (req, res) => {
+    //     db.Playlist.create({
+    //         title: req.body.playlistTitle,
+    //         string: req.body.playlistString,
+    //     })
+    //         .then(() => {
+    //             // I don't know what we'd want to do here.
+    //             res.status(200);
+    //         })
+    //         .catch(err => {
+    //             res.status(401).json(err);
+    //         });
+    // });
 
 
 };
