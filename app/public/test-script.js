2f60585f0d22686cd44cf9b1dd18ddfa5aae1c4a
Simdog <oguny006@umn.edu>
2020-07-12 21:55:22 -0500

    starting the js for add and delete functions

---
+++ app/public/test-script.js
@@ -0,0 +1,14 @@
+console.log("test");
+const tDown = $(".fa-thumbs-down");
+const tUp = $("fa-thumbs-up");
+const songRemove = $(".deletesong");
+
+$(songRemove).on("click", deleteSong)
+
+function deleteSong () {
+    
+    
+
+}
+
+
